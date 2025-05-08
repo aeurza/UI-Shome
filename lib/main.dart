5c4b693d73b265580def26b9e001250b42fb8f31
hitmeary <hitmeary@gmail.com>
2025-05-08 04:55:15 +0000

    no commets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:shome/screens/homePage.dart';
 import 'package:shome/screens/loading_screen.dart';
-import 'package:shome/screens/login.dart';
 
 void main() => runApp(const MyApp());
 
